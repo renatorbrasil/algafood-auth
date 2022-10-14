22f95273df139efecc9efbc1e0ea177606a0f107
renatorbrasil <renatorbrasil@gmail.com>
2022-10-13 21:12:42 -0300

    22.16. Configurando o Client Credentials Grant Type no Authorization Server

--- src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafoodauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,23 +21,37 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
-                .inMemory()
-                    .withClient("algafood-web")
-                    .secret(passwordEncoder.encode("web123"))
-                    .authorizedGrantTypes("password")
-                    .scopes("write", "read")
-                    .accessTokenValiditySeconds(60 * 60 * 6)
-                .and()
-                    .withClient("checkclient")
-                    .secret(passwordEncoder.encode("123"));
+            .inMemory()
+                .withClient("algafood-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
+                .refreshTokenValiditySeconds(60 * 60 * 24 * 30) // 30 dias
+            .and()
+                .withClient("faturamento")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("read")
+                .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
+            .and()
+                .withClient("checkclient")
+                .secret(passwordEncoder.encode("123"))
+
+        ;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+            .authenticationManager(authenticationManager)
+            .userDetailsService(userDetailsService);
     }
 
     @Override
