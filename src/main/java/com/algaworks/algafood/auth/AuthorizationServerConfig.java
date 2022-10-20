415ab48a5df11adc8a032fea68712030cfbcea53
renatorbrasil <renatorbrasil@gmail.com>
2022-10-20 20:24:39 -0300

    23.9. Assinando o JWT com RSA SHA-256 (chave assimétrica)

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import java.util.Arrays;
@@ -73,7 +75,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         var tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("fdafadsfadsfaf11dfasdfadsfasdfasdfasdfadsfasdfasdfasdfsadfasdfsdafasfdsafa");
+
+        var jksResource = new ClassPathResource("keystores/algafood.jks");
+        var keyStorePass = "123456";
+        var keyPairAlias = "algafood";
+
+        var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+        var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        tokenConverter.setKeyPair(keyPair);
+
         return tokenConverter;
     }
 
